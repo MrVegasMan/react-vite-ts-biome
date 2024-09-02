39c0d68324a9045ba582d966716102ba88a6b0e6
MrVegasMan <5777227@gmail.com>
2024-09-02 14:29:49 +0300

    hz this liba fool shit

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,53 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import "@radix-ui/themes/styles.css";
+import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Flex, Text, Button } from "@radix-ui/themes";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+      <Theme accentColor="red" grayColor="sage" radius="small">
+        <ThemePanel />
 
-        <button className="button" onClick={() => {}}></button>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
 
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <button className="button" onClick={() => {}}></button>
+
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+        <Flex
+          direction="column"
+          className="sm:rt-r-display-grid, sm:rt-r-gap-9"
+          gap="2"
+        >
+          <Text>Hello from Radix Themes :)</Text>
+          <Button className={"the-btn"} color={"gray"}>
+            Let's go
+          </Button>
+        </Flex>
+      </Theme>
     </>
   );
 }
