c6145171fd4a763b46583f6a54b802587cbb6712
MrVegasMan <5777227@gmail.com>
2024-09-11 19:08:11 +0300

    styles

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -9,11 +9,16 @@ function App() {
   return (
     <>
       <UserScore />
-      <div className="card">
+      <div className={"flex flex-col gap-8px"}>
+        <p className={"compact-250-numeric"}>your balance</p>
         <h2 className={"font-brand"}>{countTap}</h2>
+        <p className={"compact-300-accent-numeric text-positive-primary"}>
+          rank 1 235
+        </p>
+        <div></div>
       </div>
 
-      <div className="flex">
+      <div className={"flex"}>
         <ButtonTap onClick={() => setCountTap((count) => count + 1)} />
       </div>
     </>
